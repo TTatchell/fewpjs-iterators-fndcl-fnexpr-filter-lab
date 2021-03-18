cc6cede6ff4d8068e69d79db40595943a4c817cf
TTatchell <tim.tatchell24@gmail.com>
2021-03-18 23:07:49 +1100

    upload

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name) {
+    return array.filter(array => array.toUpperCase() === name.toUpperCase())
+}
+
+
+
+function fuzzyMatch(array, letters) {
+    return array.filter(array => array.startsWith(letters))
+
+}
+
+
+
+function matchName(array, name) {
+    return array.filter(array => array.name === name)
+
+}
+const driversX = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+console.log(matchName(driversX, "Bobby"));
+
